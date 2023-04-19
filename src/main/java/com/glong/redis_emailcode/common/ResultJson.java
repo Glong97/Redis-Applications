34a65dd09731ccb86545628367e6cab5ea57f8ba
Glong <873136927@qq.com>
2023-04-20 00:31:11 +0800

    fix 406 problem

--- src/main/java/com/glong/redis_emailcode/common/ResultJson.java
+++ src/main/java/com/glong/redis_emailcode/common/ResultJson.java
@@ -55,4 +55,28 @@ public class ResultJson {
     public static ResultJson error(String msg,Object data){
         return new ResultJson(ResultConstant.ERROR,msg,data);
     }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
 }
